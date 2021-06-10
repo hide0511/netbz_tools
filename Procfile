a8f4a1c8416c89dd30476ec112bf3560231e29ef
Your Name <you@example.com>
2021-06-10 12:21:52 +0900

    first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn netbz_tools.wsgi
---
+++ tools/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.IndexView.as_view(),name='index'),
+    path('suggest/',views.SuggestView.as_view(),name='suggest'),
+    path('heading/',views.HeadingView.as_view(),name='heading'),
+    path('chrcount/',views.ChrCountView.as_view(),name='chrcount'),
+]
